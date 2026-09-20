cb9162e20d32a4910a2f76e59a595a4ae86fea50
HV Govardhan <hvgovardhan5@gmail.com>
2026-09-20 18:09:46 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int res = 0;
+        for(int i= 0; i<s.length(); i++){
+            int sum = (123 - s[i])*(i+1);
+            res = res + sum;
+        }
+        return res;
+    }
+};
